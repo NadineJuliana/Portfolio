95806b5f58bc324c74ad8a6f3045ecbb81d307a8
NadineJuliana <nadine.juliana.b@web.de>
2026-04-28 20:13:35 +0200

    Add imprint and priivacy policy page components to folder

---
+++ src/app/features/privacy/privacypolicy-page/privacypolicy-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PrivacypolicyPage } from './privacypolicy-page';
+
+describe('PrivacypolicyPage', () => {
+  let component: PrivacypolicyPage;
+  let fixture: ComponentFixture<PrivacypolicyPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PrivacypolicyPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PrivacypolicyPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/privacy/privacypolicy-page/privacypolicy-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacypolicy-page',
+  imports: [],
+  templateUrl: './privacypolicy-page.html',
+  styleUrl: './privacypolicy-page.scss',
+})
+export class PrivacypolicyPage {
+
+}
