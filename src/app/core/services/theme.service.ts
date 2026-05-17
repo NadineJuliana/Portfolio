0661832ee10304eda74f6ebd778cb2898c07a1a1
NadineJuliana <nadine.juliana.b@web.de>
2026-05-17 16:31:24 +0200

    Expand Theme Service with hero active state

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -8,9 +8,16 @@ export class ThemeService {
   private theme = new BehaviorSubject<'light' | 'dark' | 'color'>('light');
   theme$ = this.theme.asObservable();
 
+  private heroActive = new BehaviorSubject<boolean>(true);
+  heroActive$ = this.heroActive.asObservable();
+
   setTheme(theme: 'light' | 'dark' | 'color') {
     this.theme.next(theme);
     document.body.classList.remove('theme-light', 'theme-dark', 'theme-color');
     document.body.classList.add(`theme-${theme}`);
+  }
+
+  setHeroActive(active: boolean) {
+    this.heroActive.next(active);
   }
 }
