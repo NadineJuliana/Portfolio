bd5138ebb8756d8c0492eb78833367db883a05d0
NadineJuliana <nadine.juliana.b@web.de>
2026-05-24 22:46:18 +0200

    Implement side navigation component with theme management and section scrolling

--- src/app/layout/shared/side-navigation/side-navigation.ts
+++ src/app/layout/shared/side-navigation/side-navigation.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ThemeService } from '../../../core/services/theme.service';
 
 @Component({
   selector: 'app-side-navigation',
@@ -7,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrl: './side-navigation.scss',
 })
 export class SideNavigation {
+  private themeService = inject(ThemeService);
 
+  activeSection = this.themeService.activeSection;
+  theme = this.themeService.theme;
+
+  sections = [
+    'hero',
+    'about',
+    'skills',
+    'projects',
+    'references',
+    'contact'
+  ];
+
+  scrollToSection(sectionId: string) {
+    document.getElementById(sectionId)?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  getImage(section: string) {
+    const isActive = this.activeSection() === section;
+    const theme = this.theme();
+    if (isActive) {
+      return theme === 'dark'
+        ? 'icons/square_green.svg'
+        : 'icons/square_pink.svg';
+    }
+    if (theme === 'light') {
+      return 'icons/circle_black.svg';
+    }
+    return 'icons/circle_white.svg';
+  }
 }
