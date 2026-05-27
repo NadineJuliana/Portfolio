f7a3cb91f27959be6075747c1c3fce9d0e008568
NadineJuliana <nadine.juliana.b@web.de>
2026-05-27 15:28:18 +0200

    Import projects datas and add next/previous Project functions to switch between the rendered projects

--- src/app/features/projects/projects-section/projects-section.ts
+++ src/app/features/projects/projects-section/projects-section.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PROJECTS } from './data/projects.data';
 
 @Component({
   selector: 'app-projects-section',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './projects-section.scss',
 })
 export class ProjectsSection {
+  projects = PROJECTS;
 
+  currentIndex = 0;
+
+  get currentProject() {
+    return this.projects[this.currentIndex];
+  }
+
+  nextProject() {
+    this.currentIndex =
+      (this.currentIndex + 1) % this.projects.length;
+  }
+
+  previousProject() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.projects.length)
+      % this.projects.length;
+  }
 }
