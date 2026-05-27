a344fb7d993039f9071e554a66df5cade1efb164
NadineJuliana <nadine.juliana.b@web.de>
2026-05-27 15:26:37 +0200

    Add interface Project on projects.ts

--- src/app/features/projects/interfaces/project.ts
+++ src/app/features/projects/interfaces/project.ts
@@ -1,2 +1,13 @@
 export interface Project {
+  image: string;
+  backgroundColor: string;
+  accentIcon: string;
+
+  title: string;
+  emoji: string;
+  languages: string;
+  description: string;
+
+  github: string;
+  liveTest: string;
 }
