5bee6da20d8f8e8e0c39438324bc5bd57fd1b315
NadineJuliana <nadine.juliana.b@web.de>
2026-05-31 16:37:35 +0200

    Add open/close menu functions and Output eventemtiier for burger menu on header-component.ts

--- src/app/layout/components/header/header/header.ts
+++ src/app/layout/components/header/header/header.ts
@@ -1,21 +1,32 @@
 import { NgClass } from '@angular/common';
 import { ThemeService } from './../../../../core/services/theme.service';
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, EventEmitter, inject, Output } from '@angular/core';
+import { BurgerMenu } from '../../../../shared/burger-menu/burger-menu';
 
 @Component({
   selector: 'app-header',
-  imports: [NgClass],
+  imports: [NgClass, BurgerMenu],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+  @Output() menuToggle = new EventEmitter<void>();
   private themeService = inject(ThemeService);
   readonly theme = this.themeService.theme;
   readonly isHeroSection = this.themeService.heroActive;
+  isMenuOpen = false;
 
   currentLang: 'de' | 'en' = 'de';
 
   setLanguage(lang: 'de' | 'en') {
     this.currentLang = lang;
+  }
+
+  openMenu() {
+    this.isMenuOpen = true;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
   }
 }
