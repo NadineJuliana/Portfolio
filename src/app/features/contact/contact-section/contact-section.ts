db4c70265d8ba16343fbb11a5b0c81329accca3a
NadineJuliana <nadine.juliana.b@web.de>
2026-06-02 02:11:58 +0200

    Import ContactForm on contact-section.ts

--- src/app/features/contact/contact-section/contact-section.ts
+++ src/app/features/contact/contact-section/contact-section.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Footer } from '../../../layout/components/footer/footer/footer';
+import { ContactForm } from '../../../shared/contact-form/contact-form';
 
 @Component({
   selector: 'app-contact-section',
-  imports: [],
+  imports: [Footer, ContactForm],
   templateUrl: './contact-section.html',
   styleUrl: './contact-section.scss',
 })
