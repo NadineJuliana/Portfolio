5d8de038e009454904d1021c930b9ea6261acbd9
NadineJuliana <nadine.juliana.b@web.de>
2026-06-02 02:18:46 +0200

    Add new FormGroup and formSubmit function on contact-form.ts

--- src/app/shared/contact-form/contact-form.ts
+++ src/app/shared/contact-form/contact-form.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-form.html',
   styleUrl: './contact-form.scss',
 })
 export class ContactForm {
 
+  contactForm = new FormGroup({
+    fullname: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(5)]
+    }),
+    mail: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    message: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(10)],
+    }),
+    check: new FormControl(false, {
+      validators: [Validators.requiredTrue],
+    }),
+  })
+
+  formSubmit() {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.contactForm.value);
+
+    this.contactForm.reset({
+      fullname: '',
+      mail: '',
+      message: '',
+      check: false,
+    });
+  }
 }
