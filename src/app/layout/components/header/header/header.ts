487685c2b0ad06940482621f14c8bc6a4a0edd86
NadineJuliana <nadine.juliana.b@web.de>
2026-07-02 19:56:21 +0200

    Import translation service an enhanced setLanguage function with translation service

--- src/app/layout/components/header/header/header.ts
+++ src/app/layout/components/header/header/header.ts
@@ -2,6 +2,7 @@ import { NgClass } from '@angular/common';
 import { ThemeService } from './../../../../core/services/theme.service';
 import { Component, computed, EventEmitter, inject, Output } from '@angular/core';
 import { BurgerMenu } from '../../../../shared/burger-menu/burger-menu';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -12,14 +13,20 @@ import { BurgerMenu } from '../../../../shared/burger-menu/burger-menu';
 export class Header {
   @Output() menuToggle = new EventEmitter<void>();
   private themeService = inject(ThemeService);
+  private translate = inject(TranslateService);
   readonly theme = this.themeService.theme;
   readonly isHeroSection = this.themeService.heroActive;
   isMenuOpen = false;
 
   currentLang: 'de' | 'en' = 'de';
 
+  constructor() {
+    this.translate.use(this.currentLang);
+  }
+
   setLanguage(lang: 'de' | 'en') {
     this.currentLang = lang;
+    this.translate.use(lang);
   }
 
   openMenu() {
