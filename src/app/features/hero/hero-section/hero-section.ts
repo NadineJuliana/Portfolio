659d94e452f41aebcca6bf443383ec97e627617b
NadineJuliana <nadine.juliana.b@web.de>
2026-07-02 19:57:18 +0200

    Add translation serice on hero section

--- src/app/features/hero/hero-section/hero-section.ts
+++ src/app/features/hero/hero-section/hero-section.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.scss',
 })
