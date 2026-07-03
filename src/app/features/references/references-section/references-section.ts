42d707af93eb61f8572c87d8eff1d35aeb542a61
NadineJuliana <nadine.juliana.b@web.de>
2026-07-03 13:07:00 +0200

    Add translation pipe on references section

--- src/app/features/references/references-section/references-section.ts
+++ src/app/features/references/references-section/references-section.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-references-section',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './references-section.html',
   styleUrl: './references-section.scss',
 })
