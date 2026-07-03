5f3b440483509db51a797314c7a57c742df75f44
NadineJuliana <nadine.juliana.b@web.de>
2026-07-03 12:23:14 +0200

    Add translation pipe on about me section

--- src/app/features/about-me/about-me-section/about-me-section.ts
+++ src/app/features/about-me/about-me-section/about-me-section.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me-section',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './about-me-section.html',
   styleUrl: './about-me-section.scss',
 })
