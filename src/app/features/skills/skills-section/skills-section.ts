3367dd373540976ca958ee612ca9c0d660f92fb0
NadineJuliana <nadine.juliana.b@web.de>
2026-07-03 12:43:13 +0200

    Add translation pipe on skills section

--- src/app/features/skills/skills-section/skills-section.ts
+++ src/app/features/skills/skills-section/skills-section.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skills-section',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './skills-section.html',
   styleUrl: './skills-section.scss',
 })
