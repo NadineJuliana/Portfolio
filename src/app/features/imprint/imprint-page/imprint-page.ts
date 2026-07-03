7e2301161cb12fae547d4a6009bed76eb3d05713
NadineJuliana <nadine.juliana.b@web.de>
2026-07-03 17:47:37 +0200

    Add basic layout with header and footer for imprin page

--- src/app/features/imprint/imprint-page/imprint-page.ts
+++ src/app/features/imprint/imprint-page/imprint-page.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject} from '@angular/core';
+import { Footer } from '../../../layout/components/footer/footer/footer';
+import { Header } from '../../../layout/components/header/header/header';
+import { ThemeService } from '../../../core/services/theme.service';
 
 @Component({
   selector: 'app-imprint-page',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './imprint-page.html',
   styleUrl: './imprint-page.scss',
 })
 export class ImprintPage {
+  private themeService = inject(ThemeService);
 
+  ngOnInit() {
+    this.themeService.setTheme('dark');
+    this.themeService.setHeroActive(false);
+    this.themeService.setActiveSection('imprint');
+  }
 }
+
