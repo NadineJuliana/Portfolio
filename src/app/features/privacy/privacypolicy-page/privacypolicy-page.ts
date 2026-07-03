f29639a854672536cd13115fc7474de2e8e706c1
NadineJuliana <nadine.juliana.b@web.de>
2026-07-03 17:47:53 +0200

    Add basic layout with header and footer for  privacypolicy page

--- src/app/features/privacy/privacypolicy-page/privacypolicy-page.ts
+++ src/app/features/privacy/privacypolicy-page/privacypolicy-page.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component , inject} from '@angular/core';
+import { Header } from '../../../layout/components/header/header/header';
+import { Footer } from '../../../layout/components/footer/footer/footer';
+import { ThemeService } from '../../../core/services/theme.service';
+
 
 @Component({
   selector: 'app-privacypolicy-page',
-  imports: [],
+  imports: [Header, Footer],
   templateUrl: './privacypolicy-page.html',
   styleUrl: './privacypolicy-page.scss',
 })
 export class PrivacypolicyPage {
+  private themeService = inject(ThemeService);
 
+  ngOnInit() {
+    this.themeService.setTheme('dark');
+    this.themeService.setHeroActive(false);
+    this.themeService.setActiveSection('imprint');
+  }
 }
