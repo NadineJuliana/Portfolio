899b84213e144fed2b59db56f8768fdae4feaa56
NadineJuliana <nadine.juliana.b@web.de>
2026-07-03 15:20:51 +0200

    Add translation pipe on contact section

--- src/app/features/contact/contact-section/contact-section.ts
+++ src/app/features/contact/contact-section/contact-section.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Footer } from '../../../layout/components/footer/footer/footer';
 import { ContactForm } from '../../../shared/contact-form/contact-form';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-section',
-  imports: [Footer, ContactForm],
+  imports: [Footer, ContactForm, TranslatePipe],
   templateUrl: './contact-section.html',
   styleUrl: './contact-section.scss',
 })
