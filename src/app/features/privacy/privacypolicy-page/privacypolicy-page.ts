990954609ac1b5aefda8682ea36fc798dce89e3d
NadineJuliana <nadine.juliana.b@web.de>
2026-07-04 21:36:41 +0200

    Add translation pipe on privacy policy page, import header and footer and add basic style

--- src/app/features/privacy/privacypolicy-page/privacypolicy-page.ts
+++ src/app/features/privacy/privacypolicy-page/privacypolicy-page.ts
@@ -2,11 +2,12 @@ import { Component , inject} from '@angular/core';
 import { Header } from '../../../layout/components/header/header/header';
 import { Footer } from '../../../layout/components/footer/footer/footer';
 import { ThemeService } from '../../../core/services/theme.service';
+import { TranslatePipe } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-privacypolicy-page',
-  imports: [Header, Footer],
+  imports: [Header, Footer, TranslatePipe],
   templateUrl: './privacypolicy-page.html',
   styleUrl: './privacypolicy-page.scss',
 })
