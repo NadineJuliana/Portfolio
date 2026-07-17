8c144a665f407925e24d6f1a3da80ae61a282f89
NadineJuliana <nadine.juliana.b@web.de>
2026-07-17 18:04:36 +0200

    Add toggle language, set language and get initial language functions on language service

--- src/app/core/services/language.service.ts
+++ src/app/core/services/language.service.ts
@@ -1,8 +1,35 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+export type Language = 'de' | 'en';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LanguageService {
-  
+  private translate = inject(TranslateService);
+
+  readonly currentLanguage = signal<Language>(this.getInitialLanguage());
+
+  constructor() {
+    this.translate.setFallbackLang('de');
+    this.translate.use(this.currentLanguage());
+  }
+
+  toggleLanguage(): void {
+    const language = this.currentLanguage() === 'de' ? 'en' : 'de';
+    this.setLanguage(language);
+  }
+
+  setLanguage(language: Language): void {
+    this.currentLanguage.set(language);
+    this.translate.use(language);
+    localStorage.setItem('language', language);
+  }
+
+  private getInitialLanguage(): Language {
+    const savedLanguage = localStorage.getItem('language');
+
+    return savedLanguage === 'en' ? 'en' : 'de';
+  }
 }
